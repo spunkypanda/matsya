b99e3dab0480ce9093054e264dc0911f6c540ca4
Chinmay Bag <chinmay2893@gmail.com>
2023-03-01 16:47:05 +0530

    Copy some more rpc util methods

--- src/rpc/utils.go
+++ src/rpc/utils.go
@@ -2,9 +2,27 @@ package rpc
 
 import (
 	"math/big"
+	"regexp"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/shopspring/decimal"
 )
+
+// Source:
+// https://github.com/miguelmota/ethereum-development-with-go-book/blob/master/code/util/util.go
+
+// IsValidAddress validate hex address
+func IsValidAddress(iaddress interface{}) bool {
+	re := regexp.MustCompile("^0x[0-9a-fA-F]{40}$")
+	switch v := iaddress.(type) {
+	case string:
+		return re.MatchString(v)
+	case common.Address:
+		return re.MatchString(v.Hex())
+	default:
+		return false
+	}
+}
 
 // ToDecimal wei to decimals
 func ToDecimal(ivalue interface{}, decimals int) decimal.Decimal {
@@ -21,4 +39,29 @@ func ToDecimal(ivalue interface{}, decimals int) decimal.Decimal {
 	result := num.Div(mul)
 
 	return result
+}
+
+// ToWei decimals to wei
+func ToWei(iamount interface{}, decimals int) *big.Int {
+	amount := decimal.NewFromFloat(0)
+	switch v := iamount.(type) {
+	case string:
+		amount, _ = decimal.NewFromString(v)
+	case float64:
+		amount = decimal.NewFromFloat(v)
+	case int64:
+		amount = decimal.NewFromFloat(float64(v))
+	case decimal.Decimal:
+		amount = v
+	case *decimal.Decimal:
+		amount = *v
+	}
+
+	mul := decimal.NewFromFloat(float64(10)).Pow(decimal.NewFromFloat(float64(decimals)))
+	result := amount.Mul(mul)
+
+	wei := new(big.Int)
+	wei.SetString(result.String(), 10)
+
+	return wei
 }
